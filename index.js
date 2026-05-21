c110835f90a1a61d74e8a5b0b5155280a292e4fb
Kykizi <lolkeklol098123321@gmail.com>
2026-05-21 21:58:54 +0500

    Подключила базу Render

--- index.js
+++ index.js
@@ -98,6 +98,8 @@ app.get('/news', async (req, res) => {
   }
 })
 
-app.listen(5000, () => {
-  console.log('Server started on port 5000')
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`)
 })
