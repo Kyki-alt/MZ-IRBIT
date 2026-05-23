740529a2f615d1ddad27c3884ce4a02dd130790e
Kykizi <lolkeklol098123321@gmail.com>
2026-05-23 19:58:46 +0500

    fix payment success

--- controllers/payment.controller.js
+++ controllers/payment.controller.js
@@ -2,46 +2,26 @@ const crypto = require('crypto')
 
 const pool = require('../db/db')
 
-const webmoneyResult = async (req, res) => {
-
+export const webmoneyResult = async (req, res) => {
   try {
-
     console.log('WEBMONEY BODY:', req.body)
 
     const orderId =
       Number(req.body.LMI_PAYMENT_NO)
 
-    console.log('ORDER ID:', orderId)
-
-    const result = await pool.query(
-
+    await pool.query(
       `
       UPDATE orders
-      SET payment_status = 'paid'
+      SET status = 'paid'
       WHERE id = $1
-      RETURNING *
       `,
-
       [orderId]
     )
 
-    console.log(
-      'UPDATED ROWS:',
-      result.rowCount
-    )
-
-    console.log(
-      'UPDATED ORDER:',
-      result.rows
-    )
-
-    res.send('YES')
-
+    return res.send('YES')
   } catch (error) {
-
     console.log(error)
-
-    res.status(500).send('ERROR')
+    return res.status(500).send('ERROR')
   }
 }
 
