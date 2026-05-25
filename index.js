13ba13bbe303d8af380d756a0051e38a0702b352
Kykizi <lolkeklol098123321@gmail.com>
2026-05-26 04:51:05 +0500

    fix redirects copy for render linux

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const path = require('path')
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 
 const pool = require('./db/db')
 
