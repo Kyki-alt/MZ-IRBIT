40a27501d7ce985cc4ad438105edd400cd8fa90f
Kykizi <lolkeklol098123321@gmail.com>
2026-05-27 02:03:25 +0500

    работа с формой

--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -16,5 +16,18 @@ router.post(
   createOrder
 )
 
+router.get('/', async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT *
+      FROM orders
+      ORDER BY id DESC
+    `)
+
+    res.json(result.rows)
+  } catch (e) {
+    res.status(500).json({ error: 'Ошибка получения заказов' })
+  }
+})
 
 module.exports = router
